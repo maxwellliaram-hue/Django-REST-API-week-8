84d79af381987c754eebb81f6dd9f3e3c4dd7527
Maxwell Liaram <maxwellliaram@gmail.com>
2026-08-20 15:04:26 +0300

    admin.py

--- admin.py
+++ admin.py
@@ -1,1 +1,10 @@
+from django.contrib import admin
 
+from .models import Expense
+
+
+@admin.register(Expense)
+class ExpenseAdmin(admin.ModelAdmin):
+    list_display = ("description", "amount", "category", "owner", "created_at")
+    list_filter = ("category", "created_at")
+    search_fields = ("description", "owner__username")
